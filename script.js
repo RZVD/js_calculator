ec9a6e14844a2c36d0a503d2a05efcfb123d5e4f
RZVD <razvanfuca@gmail.com>
2022-07-10 23:48:05 +0300

    Calculator outline & Operation functions

---
+++ script.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+    return a + b;   
+}
+function subtract(a, b){
+    return a - b;
+}
+function multiply(a, b){
+    return a * b;
+}
+function divide(a, b){
+    return a / b;    
+}
+
+function operate(a, b, operation){
+    let answer;
+    switch (operation) {
+        case '+':
+            answer = add(a,b);
+            break;
+        case '-':
+            answer = subtract(a,b);
+            break;
+        case '*':
+            answer = multiply(a,b);
+            break;
+        case '/':
+            answer = divide(a,b);
+            break;
+    }
+    return answer;
+}
